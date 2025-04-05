64b9b6f8239afde3a2d3edeae417341d18b1575a
Pedro Lima <goubulsegue@gmail.com>
2025-04-05 10:14:06 -0300

    feat: update language attribute in HTML and enhance Contato page layout

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,51 +1,70 @@
 const Contato = () => {
-    return (
-      <div className="container mt-5">
-        <h2 className="mb-4">Entre em Contato</h2>
-        <p className="lead">
-          Estamos aqui para ajudar! Preencha o formulário abaixo ou entre em
-          contato conosco através das informações fornecidas.
-        </p>
-        <form>
-          <div className="mb-3">
-            <label htmlFor="nome" className="form-label">
-              Nome
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="nome"
-              placeholder="Digite seu nome"
-            />
+  return (
+    <div className="container mt-5">
+      <div className="card shadow bg-tertiary">
+        <div className="card-body bg-secondary text-white">
+          <h1 className="card-title text-center mb-4">Contato</h1>
+          <h2 className="card-title mb-4">Entre em Contato</h2>
+          <p className="card-text lead">
+            Estamos aqui para ajudar! Preencha o formulário abaixo ou entre em
+            contato conosco através do WhatsApp.
+          </p>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="nome" className="form-label">
+                Nome
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="nome"
+                placeholder="Digite seu nome"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Digite seu email"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="mensagem" className="form-label">
+                Mensagem
+              </label>
+              <textarea
+                className="form-control"
+                id="mensagem"
+                rows="4"
+                placeholder="Digite sua mensagem"
+              ></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Enviar
+            </button>
+          </form>
+          <hr className="my-4" />
+          <p className="card-text text-center">
+            Ou entre em contato diretamente pelo nosso WhatsApp:
+          </p>
+          <div className="text-center">
+            <a
+              href="https://wa.link/ex2wlq"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-success"
+            >
+              Fale Conosco no WhatsApp
+            </a>
           </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Digite seu email"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="mensagem" className="form-label">
-              Mensagem
-            </label>
-            <textarea
-              className="form-control"
-              id="mensagem"
-              rows="4"
-              placeholder="Digite sua mensagem"
-            ></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Enviar
-          </button>
-        </form>
+        </div>
       </div>
-    );
-  };
-  
-  export default Contato;   
+    </div>
+  );
+};
+
+export default Contato;
